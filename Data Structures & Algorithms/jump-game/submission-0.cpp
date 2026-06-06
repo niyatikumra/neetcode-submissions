40a300e391d45560563b2a26311b59272940bf8e
niyati kumra <niyatikumra@gmail.com>
2026-06-06 21:05:20 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int maxreach=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i>maxreach) return false;
+            maxreach=max(maxreach,i+ nums[i]) ;
+            if(maxreach>=n-1) return true;
+
+        }
+        return true;
+        
+    }
+};
