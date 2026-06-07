dd2f80c05d4497873f8e3a632dc1b837ea3c4311
niyati kumra <niyatikumra@gmail.com>
2026-06-07 10:28:57 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        int curr=0;
+        int far=0;
+        int jump=0;
+        for(int i=0;i<n-1;i++)
+        {
+            far=max(far,i+nums[i]);
+            if(i==curr)
+            {
+                jump++;
+                curr=far;
+            }
+            if(curr>=n-1)
+            {
+                break;
+            }
+        }
+        return jump;
+        
+    }
+};
